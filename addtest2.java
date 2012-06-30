5a450cb2e4c19df196223783526a1ff9b73f3823
S J Saravanan <sjs7007@gmail.com>
2012-07-01 00:19:55 +0530

    missed uploading

---
+++ addtest2.java
@@ -0,0 +1,36 @@
+//addition part
+//only input and sepration
+// + addition
+import java.io.*;
+
+class addtest2
+{
+	public static void main(String args[]) throws IOException
+	{
+		DataInputStream ip = new DataInputStream(System.in);
+		String S1 = new String();
+		String temp;
+		String S2= new String();
+		int i,smaller; //smaller strores length of smaller number of s1 or s2
+		System.out.print("Enter expression : ");
+		temp = ip.readLine();
+		for(i=0;i<temp.length();i++)
+		{
+			if(temp.charAt(i)=='+')
+			{
+				S1=temp.substring(0,i); //i because of gorm [,)
+				S2=temp.substring(i+1,temp.length());
+			}
+		}
+		System.out.print(S1+" "+S2);
+		//find s1 is greater or s2
+		if(S1.length()>=S2.length())
+		{
+			smaller=S2.length();
+		}
+		for(i=S2.length();i>=0;i--)
+		{
+			
+		}
+	}
+}
